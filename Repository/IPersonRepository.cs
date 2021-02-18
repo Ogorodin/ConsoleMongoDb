200e4cfe9236f983566cc75898106f786074484c
Igor <igorvico@hotmail.com>
2021-02-18 16:17:38 +0100

    DI for Persons repo changed

---
+++ Repository/IPersonRepository.cs
@@ -0,0 +1,17 @@
+﻿using ConsoleMongoDb.Entity;
+using System.Collections.Generic;
+
+namespace ConsoleMongoDb.Repository
+{
+    interface IPersonRepository
+    {
+        public void InsertRecord(Person record);
+        public List<Person> LoadRecords();
+        public Person FindPersonById(string id);
+        public List<Person> FindPersonByLastName(string lastName);
+        public bool UpdatePerson(string id, string firstName, string lastName);
+        public void UpdatePerson(string id, Person updatedPerson);
+        public bool DeleteById(string id);
+        public long DeleteByLastName(string lastName);
+    }
+}
---
+++ Repository/PersonRepository.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using ConsoleMongoDb.Entity;
+using ConsoleMongoDb.Repository;
+using MongoDB.Driver;
+
+namespace ConsoleMongoDb.Helpers
+{
+    public class PersonRepository : IPersonRepository
+    {
+        private readonly IMongoCollection<Person> _personCollection;
+
+        public PersonRepository(string database)
+        {
+            var client = new MongoClient();
+            IMongoDatabase db = client.GetDatabase(database);
+            _personCollection = db.GetCollection<Person>(Constants.CollectionName_users);
+        }
+
+        public void InsertRecord(Person record)
+        {
+            _personCollection.InsertOne(record);
+        }
+
+        public List<Person> LoadRecords()
+        {
+            var result = _personCollection.AsQueryable().ToList();
+            return result;
+        }
+
+        public Person FindPersonById(string id)
+        {
+            try
+            {
+                var findFilter = Builders<Person>.Filter.Eq(person => person.Id, Guid.Parse(id));
+                Person person = _personCollection.Find(findFilter).FirstOrDefault();
+                return person;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        public List<Person> FindPersonByLastName(string lastName)
+        {
+            return _personCollection.Find(person => person.LastName.Contains(lastName)).ToList();
+        }
+
+        public bool UpdatePerson(string id, string firstName, string lastName)
+        {
+            try
+            {
+                var findFilter = Builders<Person>.Filter.Eq(Constants.PersonID_fieldName, Guid.Parse(id));
+                var update = Builders<Person>.Update.Set(Constants.PersonFirstName_fieldName, firstName)
+                    .Set(Constants.PersonLastName_fieldName, lastName);
+                return _personCollection.UpdateOne(findFilter, update).ModifiedCount > 0;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public void UpdatePerson(string id, Person updatedPerson)
+        {
+            try
+            {
+                _personCollection.ReplaceOne(p => p.Id == Guid.Parse(id), updatedPerson);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("EXCEPTION THROWN!");
+            }
+        }
+
+        public bool DeleteById(string id)
+        {
+            var deleteFilter = Builders<Person>.Filter.Eq(person => person.Id, Guid.Parse(id));
+            return _personCollection.DeleteOne(deleteFilter).DeletedCount > 0;
+        }
+
+        public long DeleteByLastName(string lastName)
+        {
+            long count = 0;
+            while (_personCollection.DeleteOne(person => person.LastName.Contains(lastName)).DeletedCount > 0)
+            {
+                count++;
+            }
+            return count;
+        }
+    }
+}
